a22814b872d59bc88eca4097ad16be141f16619c
Official-Husko <27901510+Official-Husko@users.noreply.github.com>
2024-03-21 04:26:20 +0100

    reworked ClosePatch.cs
    refactor: small code changes
    refactor: add comments

---
+++ FalloutCurrencies/ClosePatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using RimWorld;
+
+namespace FalloutCurrencies
+{
+    [HarmonyPatch(typeof(TradeSession), "Close")]
+    public static class ClosePatch
+    {
+        /// <summary>
+        /// Patches <see cref="TradeSession.Close"/> to restore the default currency after a trade session.
+        /// This is necessary because the <see cref="TradeSession"/> does not reset the currency
+        /// to the default after the trade session is closed.
+        /// </summary>
+        public static void Prefix()
+        {
+            // If the default currency is not Silver, then restore Silver as the currency
+            if (CurrencyManager.DefaultCurrencyDef != ThingDefOf.Silver)
+            {
+                // Restore Silver as the currency
+                CurrencyManager.SwapCurrency(ThingDefOf.Silver);
+            }
+        }
+    }
+}
